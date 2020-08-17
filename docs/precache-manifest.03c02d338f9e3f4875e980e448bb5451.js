c559eb45ac63853372f71d2cabfab9059a5038f0
Emilio Lopez <em.lopezcortes@gmail.com>
2020-08-17 19:58:29 -0400

    Docs agregados

---
+++ docs/precache-manifest.03c02d338f9e3f4875e980e448bb5451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb02c162e0ebd82e94f07c59049b01ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7cacf2d647f82ace6dc",
+    "url": "/static/css/main.827b93f5.chunk.css"
+  },
+  {
+    "revision": "15388d24d9f3484dd911",
+    "url": "/static/js/2.a11a1de1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a11a1de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cacf2d647f82ace6dc",
+    "url": "/static/js/main.f95dc471.chunk.js"
+  },
+  {
+    "revision": "796a782c5e8fbaafd491",
+    "url": "/static/js/runtime-main.b78293d5.js"
+  }
+]);
